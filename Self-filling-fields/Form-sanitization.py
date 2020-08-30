071d4733fa46ee6b5510344b9a765c1798871604
Jorge Taberoa <taberoajorge@gmail.com>
2020-08-29 20:40:55 -0500

    repository initialization

---
+++ Self-filling-fields/Form-sanitization.py
@@ -0,0 +1,59 @@
+arrayKeys = ["Name", "Last Name", "Username",
+             "Email", "Phone", "Date", "Time", "Page URL"]
+
+text = '<div dir="ltr">Esto es una prueba<div><br></div><div><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Name: Jorge Alexander Rico</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Last Name: Taberoa</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Username: taberoajorge</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Email: <a href="mailto:test@test.com">jtaberoa@ambit.com.mx</a></span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Phone: (+52) 1 5542214443</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">---</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Date: agosto 15, 2020</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Time: 2:12 am</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Page URL: </span><a target="_blank" href="https://e-rest.restbar.com.mx/empezar-a-vender/" style="color:rgb(0,135,132); display:inline-block; font-family:Roboto; font-size:13px">https://e-rest.restbar.com.mx/empezar-a-vender/</a><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">User Agent: Mozilla/5.0 (Linux; Android 8.0.0; ANE-LX3 Build/HUAWEIANE-LX3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.91 Mobile Safari/537.36</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Remote IP: 201.162.241.163</span><br style="color:rgb(102,102,102); font-family:Roboto; font-size:13px"><span style="color:rgb(102,102,102); font-family:Roboto; font-size:13px">Powered by: Elementor</span>  <br clear="all"><div><br></div>-- <br><div dir="ltr"><div dir="ltr"><div><div dir="ltr"><p style=margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><b style="font-size:12pt"><span style="font-family:Calibri,sans-serif; color:gray">VICTORIA LOESA</span></b><br></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><b><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"> </span></b><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span style="font-size:9pt; font-family:Calibri,sans-serif; color:gray">AMBIT TECHNOLOGY</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)">LAGO ESTEFANÍA 31, 4to. </span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)">PISO<br>COL. GRANA</span><span style="color:rgb(127,127,127); font-family:Calibri,sans-serif; font-size:9pt">DA, CP. 11520, CIUDAD DE MÉXICO</span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)"><img src="https://drive.google.com/a/ambit.com.mx/uc?id=1OZDUhvnqKGPCiDEf1MSi2ubPtvVaE3Mm&amp;export=download" width="96" height="86" style="font-family:Arial,Helvetica,sans-serif; font-size:small; color:rgb(34,34,34)"><br></span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:red">T.</span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)"> (55) 1054.1023 / </span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:red">F.</span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)"> (55) 1054.1024</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-bottom:12pt; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:red">M.</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:gray"> </span><u><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:gray"><a href="mailto:vloesa@ambit.com.mx" style="color:rgb(17,85,204)" target="_blank"><span style="color:gray">vloesa@ambit.com.mx</span></a></span></u><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(127,127,127)"> / </span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:red">W. </span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:gray"><a href="http://www.ambit.com.mx/" style="color:rgb(17,85,204)" target="_blank"><span style="color:gray">www.ambit.com.mx</span></a></span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span style=\'font-size:11pt; font-family:"Lucida Grande"; color:rgb(31,73,125)\'></span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span lang="ES" style="font-size:7.5pt; color:rgb(127,127,127)"><br></span><span lang="ES" style="font-size:9pt; font-family:Calibri,sans-serif; color:rgb(0,32,96)">"EXCELENTE <b>ACTITUD</b>, EXCELENTE <b>SERVICIO</b>"</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span lang="ES" style="font-size:10pt; font-family:Arial,sans-serif; color:rgb(166,166,166)">_________________________________________________________________________________________________</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><span lang="ES" style="font-size:7pt; font-family:Arial,sans-serif; color:gray>Este mensaje (y cualquier archivo adjunto) puede contener información privilegiada/confidencial. Si usted no es el destinatario indicado en este mensaje no está autorizado a copiar o entregar este mensaje a nadie mas. En tal caso, debe destruir éste mensaje y notificar al remitente vía correo electrónico, a la dirección indicada.</span><span style="font-size:11pt; font-family:Calibri,sans-serif; color:rgb(33,33,33)"><u></u><u></u></span></p><p style=\'margin-right:0cm; margin-left:0cm; font-size:12pt; font-family:"Times New Roman",serif; color:black\'><spanlang="EN-US"style="font-size:7pt; font-family:Arial,sans-serif; color:gray">This message (and it´s attachments) may have privileged/confidential information. If this message was not intended for you, then you are not authorized to copy or forward this message to anyone else. In such case, you should delete this message and notify the sender by e-mail.</span></p></div></div></div></div></div></div>\''
+ 
+dictionary = {}
+positions_final = []
+positions_initial = []
+i = 0
+e = 0
+
+def remove_tags(text):
+    tag = False
+    quote = False
+    out = ""
+    for c in text:
+        if c == '<' and not quote:
+            out = out + " "
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif (c == '"' or c == "'") and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+    return out
+
+
+def dictionary_create(array, text):
+    for i in range(len(array)):
+        if array[i] in text:
+            init = text.find(array[i])
+            last = init + len(array[i])
+            complete = text[init:last]
+            init_user = last + 1
+
+def dictionary_create(array, text):
+    for i in range(len(array)):
+        if array[i] in text:
+            init = text.find(array[i])
+            last = init + len(array[i])
+            complete = text[init:last]
+            init_user = last + 1
+            positions_initial.append(init_user)
+            if i < 7:
+                last_user = text.find(array[i+1])
+            if i == 7:
+                dictionary["Page URL"] = "https://e-rest.restbar.com.mx/empezar-a-vender/"
+                break
+            positions_final.append(last_user)
+            complete_user = text[positions_initial[i]:positions_final[i]]
+            dictionary[complete] = complete_user
+            i += 1
+
+
+if __name__ == "__main__":
+    sanitize_message = remove_tags(text)
+    dictionary_create(arrayKeys, sanitize_message)
+    print(dictionary)
